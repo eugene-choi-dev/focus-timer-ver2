02aa6b39f36bd3dae7b652ca4201a656daf11101
Eugene Choi <eugene.eugene@gmail.com>
2022-06-14 19:40:55 -0700

    npm run build

---
+++ build/precache-manifest.c1efe1e2538459f60880b03eeba61a73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe50ecf1a8c5c96f67fb2c7d05a9a45b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffe0916635c04a0c6d8b",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe0916635c04a0c6d8b",
+    "url": "/static/js/main.54b11e92.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
